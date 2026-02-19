ed37389504894083ac22f4c7149dfcc7814cef48
rishi9393 <justforuse2323@gmail.com>
2026-02-19 16:57:27 +0530

    mini chnages

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,54 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer, WebSocket } from "ws";
 
 const wss = new WebSocketServer({ port: 8000 });
 
 let count = 0;
-let allSockets = [];
+// let allSockets: WebSocket[] = [];
+
+interface User {
+  socket: WebSocket;
+  room: string;
+}
+
+let allSockets: User[] = [];
 
 wss.on("connection", (socket) => {
-  allSockets.push(socket);
-
-  count = count + 1;
-  console.log("the server is connected : and the count is : " + count);
-
   socket.on("message", (message) => {
-    console.log("message received" + message.toString());
-    for(let i = 0 ; i < allSockets.length;i++ ){
-      const s =  allSockets[i];
-       s?.send(message  )
+    const parseMessage = JSON.parse(message);
+    if (parseMessage.type == "join") {
+      allSockets.push({
+        socket,
+        room: parseMessage.payload.roomId,
+      });
     }
-    setTimeout(() => {
-      socket.send(message.toString() +" "+  "message from server sended");
-    },1000);
+
+    if (parseMessage.type == "chat") {
+      // const currentUerRoom =  allSockets.find((x)=> x.socket == socket).room
+      let currentUerRoom = null;
+      for (let i = 0; i < allSockets.length; i++) {
+        if(allSockets[i].socket == socket){
+            currentUerRoom = allSockets[i].room;
+        }
+      }
+    }
   });
+
+  socket.on("disconnect", () => {
+    allSockets = allSockets.filter((x) => x != socket);
+  });
+
+  // allSockets.push(socket);
+
+  // count = count + 1;
+  // console.log("the server is connected : and the count is : " + count);
+
+  // socket.on("message", (message) => {
+  //   console.log("message received" + message.toString());
+  //   for (const s of allSockets) {
+  //     s.send(message.toString() + ": send from the server ");
+  //   }
+  //   setTimeout(() => {
+  //     socket.send(message.toString() + " " + "message from server sended");
+  //   }, 1000);
+  // });
 });
